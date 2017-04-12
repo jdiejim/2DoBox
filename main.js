20b8133bf8ad2b8feb4f894b6d5e77850e7b15a3
Juan Diego Jimenez <jdiejim@gmail.com>
2017-04-11 21:50:01 -0600

    add importance filter fn

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// TODO: ask if filter should be label
+
 // ----- Setup -----
 prependTodos(getTodos());
 validateSaveButton();
@@ -7,7 +9,8 @@ function Todo(inputs) {
   this.title = inputs.title;
   this.task = inputs.task;
   this.id = inputs.id;
-  this.quality = "swill";
+  this.importance = "Normal";
+  this.level = 2;
 }
 
 // ----- Events Listeners -----
@@ -15,6 +18,7 @@ $('.save-button').on('click', saveTodo);
 $('.save-button').on('keyup', saveTodo);
 $('.inputs').on('keyup', validateSaveButton);
 $('.search-bar').on('keyup', searchTodos);
+$('.importance').on('click', '.importance-btn', filterByImportance);
 $('.card-container').on('click', '.delete', deleteTodo)
                     .on('click', '.up-vote', upVote)
                     .on('click', '.down-vote', downVote)
@@ -35,22 +39,30 @@ function deleteTodo() {
   storeTodo(removeFromTodos($(this).parent().prop('id')))
   $(this).parent().remove()
 }
-
+// TODO: refactor upvote
+// TODO: refactor upQuality
 function upVote() {
   var todos = getTodos();
   var index = getTodoIndex($(this).parents('.todo-card').prop('id'));
   var todo = todos[index];
-  todo.quality = upQuality(todo.quality);
+  if (todo.level < 4) {
+    todo.level += 1;
+  }
+  todo.importance = upQuality(todo.level);
   todos[index] = todo;
   storeTodo(todos);
   $(this).parents('.todo-card').replaceWith(buildTodoElement(todo));
 }
-
+// TODO: refactor downvote
+// TODO: refactor upQuality
 function downVote() {
   var todos = getTodos();
   var index = getTodoIndex($(this).parents('.todo-card').prop('id'));
   var todo = todos[index];
-  todo.quality = downQuality(todo.quality);
+  if (todo.level > 0) {
+    todo.level -= 1;
+  }
+  todo.importance = upQuality(todo.level);
   todos[index] = todo;
   storeTodo(todos);
   $(this).parents('.todo-card').replaceWith(buildTodoElement(todo));
@@ -138,12 +150,12 @@ function buildTodoElement(todo) {
             <button type="button" class="delete"></button>
             <h2 contenteditable="true" class="title">${todo.title}</h2>
             <p contenteditable="true" class="task">${todo.task}</p>
-            <div class="quality-container">
+            <div class="importance-container">
               <button type="button" class="up-vote"></button>
               <button type="button" class="down-vote"></button>
-              <p class="todo-quality">
-                <span class="quality-font">quality: </span>
-                <span class="rating">${todo.quality}</span>
+              <p class="todo-importance">
+                <span class="importance-font">importance: </span>
+                <span class="rating">${todo.importance}</span>
               </p>
               </div>
           </article>`
@@ -158,30 +170,9 @@ function storeTodo(array) {
   localStorage.setItem('todos', JSON.stringify(array));
 }
 
-function upQuality(quality) {
-  switch (quality) {
-    case 'swill':
-      return 'plausible';
-    case 'plausible':
-      return 'genius';
-    case 'genius':
-      return 'genius';
-    default:
-      return quality;
-  }
-}
-
-function downQuality(quality) {
-  switch (quality) {
-    case 'swill':
-      return 'swill';
-    case 'plausible':
-      return 'swill';
-    case 'genius':
-      return 'plausible';
-    default:
-      return quality;
-  }
+function upQuality(level) {
+  var levels = ['None', 'Low', 'Normal', 'High', 'Critical'];
+  return levels[level];
 }
 
 function getFilteredTodos(searchValue) {
@@ -196,5 +187,20 @@ function validateSaveButton() {
     $('.save-button').prop('disabled', false);
   } else {
     $('.save-button').prop('disabled', true);
+  }
+}
+
+function getFilteredByImportance(importance) {
+  return getTodos().filter(function(todo) {
+    return todo.importance === importance;
+  });
+}
+
+function filterByImportance() {
+  var importance = $(this).prop('id');
+  if (importance !== 'Any') {
+    prependTodos(getFilteredByImportance(importance));
+  } else {
+    prependTodos(getTodos());
   }
 }
